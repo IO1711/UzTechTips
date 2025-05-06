db7d706907a5c2334b9cf4eb222dba15e8894851
IO1711 <52698195+IO1711@users.noreply.github.com>
2025-05-06 10:42:54 +0500

    crossOrigin added to controllers

--- src/main/java/com/techtips/uzbekTechTips/controller/InputController.java
+++ src/main/java/com/techtips/uzbekTechTips/controller/InputController.java
@@ -1,6 +1,7 @@
 package com.techtips.uzbekTechTips.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +15,7 @@ import com.techtips.uzbekTechTips.model.Apps;
 
 import java.util.List;
 
-
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api")
 public class InputController {
